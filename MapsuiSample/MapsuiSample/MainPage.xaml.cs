d6063f549fa40af9886f7708a7c8dac48a2a8cfc
Anton Makov <anton@rts.com>
2020-11-04 09:25:28 -0500

    Added Mapsui maps to project

--- MapsuiSample/MapsuiSample/MainPage.xaml.cs
+++ MapsuiSample/MapsuiSample/MainPage.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mapsui;
+using Mapsui.Projection;
+using Mapsui.Utilities;
 using Xamarin.Forms;
 
 namespace MapsuiSample
@@ -12,6 +15,28 @@ namespace MapsuiSample
         public MainPage()
         {
             InitializeComponent();
+            initMap();
+        }
+
+        private void initMap()
+        {
+            var map = new Map
+            {
+                CRS = "EPSG:3857",
+                Transformation = new MinimalTransformation()
+            };
+
+            var tileLayer = OpenStreetMap.CreateTileLayer();
+
+            map.Layers.Add(tileLayer);
+            map.Widgets.Add(new Mapsui.Widgets.ScaleBar.ScaleBarWidget(map)
+            {
+                TextAlignment = Mapsui.Widgets.Alignment.Center,
+                HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Left,
+                VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Bottom
+            });
+
+            mapView.Map = map;
         }
     }
 }
